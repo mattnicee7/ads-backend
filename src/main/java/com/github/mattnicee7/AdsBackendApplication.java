887422f1eef223ab176c713c820bbf97fe334907
mattnicee7 <mattxnice@gmail.com>
2024-10-28 21:34:01 -0300

    imported project

---
+++ src/main/java/com/github/mattnicee7/AdsBackendApplication.java
@@ -0,0 +1,14 @@
+package com.github.mattnicee7;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsBackendApplication.class, args);
+    }
+
+}
